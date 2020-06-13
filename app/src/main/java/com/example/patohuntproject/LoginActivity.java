1478ba9d99af4dce622102623bf002c54b2581dc
Cristian <marquezcristian@outlook.com.ar>
2020-06-13 14:55:44 -0300

    Version 1.0

---
+++ app/src/main/java/com/example/patohuntproject/LoginActivity.java
@@ -0,0 +1,53 @@
+package com.example.patohuntproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.patohuntproject.common.Constantes;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText etNick;
+    Button btnStart;
+    String nick;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etNick =findViewById(R.id.editTextNick);
+        btnStart = findViewById((R.id.buttonStart));
+
+        //Cambiar tipo de fuente
+        Typeface typeface = Typeface.createFromAsset(getAssets(),"pixel.ttf");
+        etNick.setTypeface(typeface);
+        btnStart.setTypeface(typeface);
+
+        //Eventos :evento click
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nick =etNick.getText().toString();
+
+                if(nick.isEmpty()){
+                    etNick.setError("El nombre de usuario es obligatorio");
+                } else if (nick.length() <3) {
+                    etNick.setError("Debe tener al menos 3 caracteres");
+
+                }else {
+                    //Pasamos de una activity a otra ,pero no se ejecuta hasta declarar el startActivity
+                    Intent i = new Intent(LoginActivity.this,GameActivity.class);
+                    i.putExtra(Constantes.EXTRA_NICK,nick);
+                    startActivity(i);
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/patohuntproject/common/Constantes.java
@@ -0,0 +1,5 @@
+package com.example.patohuntproject.common;
+
+public class Constantes {
+    public static final String EXTRA_NICK ="nickname";
+}
